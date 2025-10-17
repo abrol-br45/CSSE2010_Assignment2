bf6c591087669d7ca1f6f1d0731b3444986de2af
Branden Rease Abrol <s4974827@student.uq.edu.au>
2025-10-17 10:47:50 +1000

    Hardware header.

---
+++ Assignment2/hardware.h
@@ -0,0 +1,21 @@
+/*
+ * hardware.h
+ *
+ * Created: 17-10-2025 10:45:16 AM
+ *  Author: brand
+ */ 
+
+
+#ifndef HARDWARE_H_
+#define HARDWARE_H_
+#include <avr/io.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+void hardware_init(void);
+uint8_t read_mode_switch(void);
+bool button_pressed(uint8_t pin);
+void update_mode(uint8_t mode);
+void update_power(uint8_t level);
+
+#endif /* HARDWARE_H_ */
